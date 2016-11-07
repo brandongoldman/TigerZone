a33862f5f1e8bbbba679e875fd880ccff9621490
adamwexler01 <adamwexler01@gmail.com>
2016-11-07 13:37:54 -0500

    Commit #1
    Initializing classes

---
+++ src/Tile.java
@@ -0,0 +1,16 @@
+public class Tile {
+
+	//private variables for a tile
+
+
+	//public methods for tile action
+
+
+	//getters
+
+
+
+	//setters
+
+	
+}
--- src/board.java
+++ src/board.java
@@ -1,1 +1,14 @@
+public class Board {
 
+	//Private variables initializes
+
+
+	//public method
+
+
+	//getters
+
+
+	//setters
+
+}
