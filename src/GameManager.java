2bcbab6fe38a52a6d6262f0fe776b82700e6048c
alexbrucek <abkrepacki@ufl.edu>
2016-11-11 00:06:25 -0500

    Added basic functions to GameManager Class
    Game board needs to take responsibility for meeple placement and tile placement.  Additionally, added startGame and StopGame functions.

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,50 @@
-public class GameManager{
+public abstract class GameManager 
+{
+	private int numberOfPlayers;
+	private int turnNumber;
+	private int playerScore;
+  private boolean stopGameFlag;
+	
+	public GameManager() 
+	{
+		this.numberOfPlayers = 2;
+		this.turnNumber = 0;
+		this.playerScore = 0;
+    this.stopGameFlag = false;
+	}
 
+	public GameManager(int numberOfPlayers, int turnNumber, int playerScore, boolean stopGameFlag)
+	{
+		this.numberOfPlayers = numberOfPlayers;
+		this.turnNumber = turnNumber;
+		this.playerScore = playerScore;
+    this.stopGameFlag = false;
+	}
+	
+	
+	public int setScore()
+	{
+		// Need access to board to set the score
+		
+		return 1;
+	}
+
+	
+	public void startGame()
+	{
+		do
+		{
+			// Game runs in infinite loop, runs until hits flag that game is over.
+      // GameManager can stop game if realizes no more tiles can be placed.  
+			
+		} while(true);
+		
+	}
+	
+	public void stopGame()
+	{
+    stopGameFlag = true;   // game is over
+	}
+	
+	
 }
