46e07af7e43d9e23b281f1535899100784967533
David Weng <dkweng@gmail.com>
2016-11-11 13:15:28 -0500

    Update GameManager.java

--- src/GameManager.java
+++ src/GameManager.java
@@ -2,33 +2,38 @@ public abstract class GameManager
 {
 	private int numberOfPlayers;
 	private int turnNumber;
-	private int playerScore;
+	private int[] playerScore;
   	private boolean stopGameFlag;
 	
 	public GameManager() 
 	{
 		this.numberOfPlayers = 2;
 		this.turnNumber = 0;
-		this.playerScore = 0;
     		this.stopGameFlag = false;
 	}
 
-	public GameManager(int numberOfPlayers, int turnNumber, int playerScore, boolean stopGameFlag)
+	public GameManager(int numberOfPlayers, int turnNumber, boolean stopGameFlag)
 	{
 		this.numberOfPlayers = numberOfPlayers;
 		this.turnNumber = turnNumber;
-		this.playerScore = playerScore;
+		this.playerScore = new int[numberOfPlayers];
     		this.stopGameFlag = false;
 	}
 	
 	
-	public int setScore()
+	public void setScore(int Score, int PlayerNumber)
 	{
-		// Need access to board to set the score
+		PlayerScore[PlayerNumber]=Score;
 		
-		return 1;
 	}
 
+	public void ReturnMeeple(){
+        // Calculations to determine where meeples need to be returned
+
+        /** Call each player who need to meeples return and give back their meeples.
+         *  Also be sure to give players their score earned for those returned meeples.
+         */
+    	}
 	
 	public void startGame()
 	{
