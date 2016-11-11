ca853eac605d27be163b985521b0f8aabaa4f8f2
David Weng <dkweng@gmail.com>
2016-11-11 13:57:13 -0500

    Updated GameManager Added Position
    Will describe in group meeting why added position class. Refer to
    comments in Game Manager under CheckValidMove Function.

--- src/GameManager.java
+++ src/GameManager.java
@@ -4,12 +4,17 @@ public abstract class GameManager
 	private int turnNumber;
 	private int[] playerScore;
   	private boolean stopGameFlag;
+	//private Position[] OpenSpaces;
+	//private Board;
+	//private TileStack;
+
 	
 	public GameManager() 
 	{
 		this.numberOfPlayers = 2;
 		this.turnNumber = 0;
-    		this.stopGameFlag = false;
+		this.stopGameFlag = false;
+		this.playerScore = new int[numberOfPlayers]
 	}
 
 	public GameManager(int numberOfPlayers, int turnNumber, boolean stopGameFlag)
@@ -17,7 +22,7 @@ public abstract class GameManager
 		this.numberOfPlayers = numberOfPlayers;
 		this.turnNumber = turnNumber;
 		this.playerScore = new int[numberOfPlayers];
-    		this.stopGameFlag = false;
+		this.stopGameFlag = false;
 	}
 	
 	
@@ -27,13 +32,22 @@ public abstract class GameManager
 		
 	}
 
-	public void ReturnMeeple(){
+	public void returnMeeple(){
         // Calculations to determine where meeples need to be returned
 
         /** Call each player who need to meeples return and give back their meeples.
          *  Also be sure to give players their score earned for those returned meeples.
          */
     	}
+
+	public boolean CheckValidMove(){
+		/**Use the Openspaces to keep track of all open spaces in O(1) time and updae the Openspaces array everytime a
+		 * new tile is placed onto the board. Doing this will allow us to know where the new tile was places so every
+		 * iteration of a new tile being placed we only need to check if that tile is a valid move instead of entire
+		 * board.
+		 */
+		return true;
+	}
 	
 	public void startGame()
 	{
