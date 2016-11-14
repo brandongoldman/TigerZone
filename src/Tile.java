628aecdf3e52df69e03dbe94478192b5ff2b7fd4
adamwexler01 <adamwexler01@gmail.com>
2016-11-14 13:18:43 -0500

    Adding tile

--- src/Tile.java
+++ src/Tile.java
@@ -2,11 +2,16 @@ public class Tile {
 
 	//private variables for a tile
 	
-
+	int xCoordinate;
+	int yCoordinate;
 
 
 	//public methods for tile action
 
+	public Tile(int x, int y){
+		this.xCoordinate = x;
+		this.yCoordinate = y;
+	}
 
 	//getters
 
