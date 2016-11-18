f5af7accb3d6b24dbb97158f1a64d147d57eb86b
adamwexler01 <adamwexler01@ufl.edu>
2016-11-18 01:08:18 -0500

    adding functionality to HashBoard: which is board implementation using HashMap

--- src/HashBoard.java
+++ src/HashBoard.java
@@ -1,5 +1,8 @@
 import java.util.HashMap;
-
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+import java.util.Iterator;
 
 public class HashBoard{
 	
@@ -9,10 +12,60 @@ public class HashBoard{
 		gBoard = new HashMap<Position, Tile>();
 	}
 
-	public Tile addTile(Position pos, Tile tile){
+	public boolean didAddTile(Position pos, Tile tile){
+
+		//Check if the spot is available
+		if(gBoard.containsKey(pos)){
+			return false;
+		}
+
+		gBoard.put(pos, tile);
+
+		return true;
+	}
 
 
-		return new Tile(pos, tile);
+	public Set<Position> checkOpenSpots(){
+
+		//Initializing Possible spots for users
+		Set<Position> set = new HashSet<Position>();
+		
+		Set<Position> keySet = gBoard.keySet();
+		Iterator<Position> it = keySet.iterator();
+
+		while(it.hasNext()){
+
+			Position pos = it.next();
+
+			if(!gBoard.containsKey(pos.getXPosition() + 1)){
+				//Position can be added
+				Position set_pos = new Position(pos.getXPosition() + 1, pos.getYPosition());
+
+				set.add(set_pos);
+			} 
+
+			if(!gBoard.containsKey(pos.getYPosition() + 1)){
+				Position set_pos = new Position(pos.getXPosition(), pos.getYPosition() + 1);
+
+				set.add(set_pos);
+			}
+
+			if(!gBoard.containsKey(pos.getXPosition() - 1)){
+				Position set_pos = new Position(pos.getXPosition() - 1, pos.getYPosition());
+
+				set.add(set_pos);
+			}
+
+			if(!gBoard.containsKey(pos.getYPosition() - 1)){
+				Position set_pos = new Position(pos.getXPosition(), pos.getYPosition() - 1);
+
+				set.add(set_pos);
+			}
+
+		}
+
+		return set;
+
 	}
 
 }
--- src/Position.java
+++ src/Position.java
@@ -7,4 +7,21 @@ public class Position
 		this.x = x;
 		this.y = y;
 	}
+
+	public int getXPosition(){
+		return x;
+	}
+
+	public int getYPosition(){
+		return y;
+	}
+
+	public void setXPosition(int x){
+		this.x = x;
+	}
+
+	public void setYPosition(int y){
+		this.y = y;
+	}
+
 }
