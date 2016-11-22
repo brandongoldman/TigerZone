1011b2f852434e40d18d3b2a053a62707bcddca0
rorath <dkweng@gmail.com>
2016-11-22 16:43:53 -0500

    Updated FeatureArea

--- src/FeatureArea.java
+++ src/FeatureArea.java
@@ -1,16 +1,35 @@
 /**
  * Created by David on 11/22/2016.
  */
+import java.util.HashSet;
 import java.util.Set;
 
 public class FeatureArea {
     private Tiger tiger;
     private boolean hasTiger;
     private boolean completed;
-    private Set<Position> area;
+    Set<Position> area;
     private Edge typeOfArea;
 
-    public FeatureArea(){
+    public FeatureArea(int type){
+        area = new HashSet<Position>();
+        typeOfArea=new Edge(type);
+        completed = false;
+        hasTiger=false;
+    }
 
+    public void addTiger(Tiger newTiger){
+        tiger=newTiger;
+        hasTiger=true;
+    }
+
+    public Edge getTypeOfArea() {
+        return typeOfArea;
+    }
+    public boolean getCompleted(){
+        return completed;
+    }
+    public void setCompleted(boolean completed){
+        this.completed=completed;
     }
 }
