c6f70db749ca20355564f0fe192197e68fe0db89
Brandon Goldman <brandon.m.goldman@gmail.com>
2016-11-25 21:34:08 -0500

    Update BestMoveTest.java

--- tests/BestMoveTest.java
+++ tests/BestMoveTest.java
@@ -8,29 +8,6 @@ public class BestMoveTest
 		ti.interpret("TLTJ-\nTTTT-\nTJTJ-\nJJJJ-\nLLJJ-");		
 		Tile[] arr = ti.getTileArray();							
 																
-		HashBoard board = new HashBoard(2);						
-																
-															
-		board.AddTile(new Position(0,0), arr[0]);	
-		board.updateOpenSpots(new Position(0, 0));
-		board.printKeys();
-		
-		board.AddTile(new Position(0,1), arr[1]);
-		board.AddTile(new Position(0,2), arr[2]);
-		board.AddTile(new Position(-1,0), arr[3]);
-		board.AddTile(new Position(-1,1), arr[4]);
-
-
-		
-		//hm = board.getMap();
-		
-		// assertEquals(1, board.getMap().get(new Position(0, 1)).getEdgeB());
-		// assertEquals(0, board.getMap().get(new Position(0, 2)).getEdgeR());
-		// assertEquals(0, board.getMap().get(new Position(-1, 0)).getEdgeR());
-
-
-		
-		
+		HashBoard board = new HashBoard(2);		
 	}
-
 }
