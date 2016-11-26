5e2c6bf58b25c11e440461cd0ff9770c879533f3
alexbrucek <abkrepacki@ufl.edu>
2016-11-25 21:30:29 -0500

    New class MatchParam
    Will use for Match Protocol method which I'm currently working on as of Nov. 25th.

---
+++ src/MatchParam.java
@@ -0,0 +1,12 @@
+
+public class MatchParam 
+{
+	public String opponent;
+	public String startingTile;
+	public int startingTileX;
+	public int startingTileY;
+	public int numOfTiles;
+	public int time;       // seconds the time to the beginning of the Match
+	String[] tiles = new String[77];
+}
+
