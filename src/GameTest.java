df059b852ac9dac9bd624d53442c2479a04257f0
adamwexler01 <adamwexler01@ufl.edu>
2016-11-26 21:51:23 -0500

    Adding Test Suite for Gameboard in the GameTest.java class

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,27 +1,85 @@
+//	Import statements & possibly using JUnit to
+//	test the application
+
+
 public class GameTest{
 
-	public static void main(String[] args){
 
-		HashBoard board = new HashBoard();
-		Player player = new Player();
+	public static void testA(){
 
-		board.addTile();
-		player.placeTile();
+		/*
+				
+			Test A Description:
 
-		TileInterpreter ti = new TileInterpreter();
-		Tile tile = ti.interpret("TLTTP");
+			-	Playing an entire game with just tiles is possible.
+				A person/AI can place tiles on a board and the
+				board can either accept the tile on the board. This case
+				only generates items that can be accepted onto the
+				Gameboard. 
 
+			-	Test A was also created to show the functionality of the
+				Test A outputs and the tile placements that should be 
+				produced from the board
 
-
-		/*					
-			Assumptions:
-				1. 	We are player 1 for this test game
-				2. 	We get the description of the tiles
-					from the very beginning of the
-					game 
 
 		*/
 
+
+		HashBoard board = new HashBoard();
+		TileInterpreter ti = new TileInterpreter();
+		Tile tile = ti.interpret("TLTTP");
+		board.addTile(new Position(0, 1), tile);		//	Outcome: Pass
+		Tile tile2 = ti.interpret("TTTT-");
+		board.addTile(new Position(0, -1), tile2);		//	Outcome: Pass
+		Tile tile3 = ti.interpret("TJTJ-");
+		board.addTile(new Position(0, -2), tile3);		//	Outcome: Pass
+		Tile tile4 = ti.interpret("LLJJ-");
+		board.addTile(new Position(1, -2), tile4);		//	Outcome: Pass
+		Tile tile5 = ti.interpret("LLLL-");
+		board.addTile(new Position(1, 1), tile5);		//	Outcome: Pass
+		Tile tile6 = ti.interpret("JJJJ-");
+		board.addTile(new Position(1, 0), tile6);		//	Outcome: Fail
+		board.printLake();
+		board.printTrail();
+
+	}
+
+	public static void testB(){
+		HashBoard board = new HashBoard();
+		TileInterpreter ti = new TileInterpreter();
+	}
+
+	public static void testC(){
+		HashBoard board = new HashBoard();
+		TileInterpreter ti = new TileInterpreter();
+
+
+	}
+
+	public static void testD(){
+		HashBoard board = new HashBoard();
+		TileInterpreter ti = new TileInterpreter();
+
+
+	}
+
+
+	public static void main(String[] args){
+
+		/*
+
+			Performs a test suite of the most common
+			curveballs that can be thrown our way.
+			Also, the test suite begins with the
+			easiest test and then ends with a more complex
+			one
+
+		*/
+
+		testA();
+		testB();
+		testC();
+		testD();
 
 	}
 
