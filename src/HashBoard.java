ae761cc4b8c6a8da3b42e2ea7a2aa688c7ad60e8
David Weng <dkweng@gmail.com>
2016-11-29 16:29:17 -0500

    Comment out Testing function HashBoard

--- src/HashBoard.java
+++ src/HashBoard.java
@@ -359,13 +359,13 @@ public class HashBoard{
 			}
 		}
 
-		int[][] minitile = tile.getMiniZones();
+		/*int[][] minitile = tile.getMiniZones();
 		for(int i = 0; i<3; i++){
 			System.out.println();
 			for(int j = 0; j<3; j++){
 				System.out.print(minitile[i][j] + " ");
 			}
-		}
+		}*/
 
 		/**JUNGLES**/
 		//FAILED
