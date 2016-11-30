81ec7d818ec17583806d93b19ba762d5b52929d4
lazarbrad <bradleyalazar@ufl.edu>
2016-11-30 11:28:59 -0500

    Update testBusiness.java

--- tests/testBusiness.java
+++ tests/testBusiness.java
@@ -12,7 +12,7 @@ public class testBusiness {
 		
 		Tile t0 = ti.interpret("TLTJ-");
 		
-		assertEquals("GAME A MOVE 1 PLACE TLTJ- AT 1 0 180 TIGER 4\n",hb.FindBestMove(t0, true, "A", 1).toString("A", 1));
+		assertEquals("GAME A MOVE 1 PLACE TLTJ- AT 1 0 180 NONE\n",hb.FindBestMove(t0, true, "A", 1).toString("A", 1));
 		
 	}
 	
@@ -30,15 +30,33 @@ public class testBusiness {
 		assertEquals("GAME B MOVE 2 TILE LLLL- UNPLACEABLE PASS\n", hb.FindBestMove(t1, true, "B", 2).passOnTile(t1, "B", 2));		
 	}
 	
-	@Test
-	public void testTigerOnDen() {
+	/*@Test
+	public void testTigersOnDen() {
+		// THIS TEST WILL ONLY WORK IF THRESHOLD IS 0 (as of 11/30)
+		// THIS TEST WILL WORK IF DENS ARE MADE TO BE AN EXCEPTION FOR THRESHOLD BASED TIGER PLACEMENT
+		
 		TileInterpreter ti = new TileInterpreter();
 		HashBoard hb = new HashBoard();
 		
 		Tile t0 = ti.interpret("JJJJX");
-		
+
+		// FIRST TIGER PLACED DOWN
 		assertEquals("GAME A MOVE 1 PLACE JJJJX AT -1 0 0 TIGER 5\n", hb.FindBestMove(t0, true, "A", 1).toString("A", 1));	
-	}
+		hb.FindBestMove(t0, true, "A", 2).toString("A", 2);
+		hb.FindBestMove(t0, true, "A", 3).toString("A", 3);
+		hb.FindBestMove(t0, true, "A", 4).toString("A", 4);
+		hb.FindBestMove(t0, true, "A", 5).toString("A", 5);
+		hb.FindBestMove(t0, true, "A", 6).toString("A", 6);
+		// SEVENTH AND LAST TIGER PLACED DOWN
+		assertEquals("GAME A MOVE 7 PLACE JJJJX AT -3 0 0 TIGER 5\n", hb.FindBestMove(t0, true, "A", 7).toString("A", 7));
+		// CURRENTLY OUT OF TIGERS
+		assertEquals("GAME A MOVE 8 PLACE JJJJX AT -3 -1 0 NONE\n", hb.FindBestMove(t0, false, "A", 8).toString("A", 8));
+		hb.FindBestMove(t0, false, "A", 9).toString("A", 9);
+		hb.FindBestMove(t0, false, "A", 10).toString("A", 10);
+		hb.FindBestMove(t0, false, "A", 11).toString("A", 11);
+		// A TIGER HAS BEEN RETURNED 
+		assertEquals("GAME A MOVE 12 PLACE JJJJX AT -3 -2 0 TIGER 5\n", hb.FindBestMove(t0, true, "A", 12).toString("A", 12));
+	}*/
 	
 	@Test
 	public void testTigerOnLake() {
@@ -47,110 +65,15 @@ public class testBusiness {
 		
 		Tile t0 = ti.interpret("LTTTP");
 		Tile t1 = ti.interpret("JLJJ-");
-		Tile t2 = ti.interpret("JJLJ-");
+		Tile t2 = ti.interpret("LLLL-");
 		
-		//hb.FindBestMove(t0, true, "B", 4);
-		//hb.FindBestMove(t1, true, "B", 5);
-		//hb.FindBestMove(t2, true, "B", 6);
-		
-		assertEquals("GAME B MOVE 4 PLACE LTTTP AT 1 0 90 TIGER 4\n", hb.FindBestMove(t0, true, "B", 4).toString("B", 4));
-		assertEquals("GAME B MOVE 5 PLACE JLJJ- AT -1 0 90 TIGER 2\n", hb.FindBestMove(t1, true, "B", 5).toString("B", 5));
-		assertEquals("GAME B MOVE 6 PLACE JJLJ- AT -1 -1 0 TIGER 8\n", hb.FindBestMove(t2, true, "B", 6).toString("B", 6));
+		hb.FindBestMove(t0, true, "B", 4).toString("B", 4);
+		hb.FindBestMove(t1, true, "B", 5).toString("B", 5);
+		hb.FindBestMove(t2, true, "B", 6).toString("B", 6);
+		hb.FindBestMove(t2, true, "B", 7).toString("B", 7);
+		hb.FindBestMove(t2, true, "B", 7).toString("B", 8);
+		// WITH THRESHOLD OF 5 WE SHOULD PLACE A TIGER ON THE 5th LAKE SEGMENT
+		assertEquals("GAME B MOVE 9 PLACE LLLL- AT 0 3 0 TIGER 1\n", hb.FindBestMove(t2, true, "B", 7).toString("B", 9));
 	}
-	
-	/*@Test
-	public void testEntireBoard() {
-		TileInterpreter ti = new TileInterpreter();
-		HashBoard hb = new HashBoard();
-		
-		Tile t0 = ti.interpret("LTTTP");
-		Tile t1 = ti.interpret("JJJJX");
-		Tile t2 = ti.interpret("JLTTB");
-		Tile t3 = ti.interpret("TLTJD");
-		Tile t4 = ti.interpret("TLJT-");
-		Tile t5 = ti.interpret("TLTT-");
-		Tile t6 = ti.interpret("TLLLC");
-		Tile t7 = ti.interpret("LLLL-");
-		Tile t8 = ti.interpret("JLJL-");
-		Tile t9 = ti.interpret("TJTJ-");
-		Tile t10 = ti.interpret("TJTT-");
-		Tile t11 = ti.interpret("TLJTP");
-		Tile t12 = ti.interpret("TLTJ-");
-		Tile t13 = ti.interpret("TLLTB");
-		Tile t14 = ti.interpret("JJJJX");
-		Tile t15 = ti.interpret("TJTT-");
-		Tile t16 = ti.interpret("JLLL-");
-		Tile t17 = ti.interpret("TJJT-");
-		Tile t18 = ti.interpret("TJJT-");
-		Tile t19 = ti.interpret("LJJJ-");
-		Tile t20 = ti.interpret("LLJJ-");
-		Tile t21 = ti.interpret("JLLL-");
-		Tile t22 = ti.interpret("TLTTP");
-		Tile t23 = ti.interpret("TLLLC");
-		Tile t24 = ti.interpret("TJJT-");
-		Tile t25 = ti.interpret("JLLL-");
-		Tile t26 = ti.interpret("TLTJD");
-		Tile t27 = ti.interpret("TJJT-");
-		Tile t28 = ti.interpret("TLLT-");
-		Tile t29 = ti.interpret("LLJJ-");
-		Tile t30 = ti.interpret("JLLL-");
-		Tile t31 = ti.interpret("TJTT-");
-		Tile t32 = ti.interpret("TJJT-");
-		Tile t33 = ti.interpret("TJJT-");
-		Tile t34 = ti.interpret("TJTJ-");
-		Tile t35 = ti.interpret("TJTJ-");
-		Tile t36 = ti.interpret("JJJJX");
-		Tile t37 = ti.interpret("LJTJD");
-		Tile t38 = ti.interpret("LTTTP");
-		
-		hb.FindBestMove(t0, true, "a", 1);
-		hb.FindBestMove(t1, true, "a", 1);
-		hb.FindBestMove(t2, true, "a", 1);
-		hb.FindBestMove(t3, true, "a", 1);
-		hb.FindBestMove(t4, true, "a", 1);
-		hb.FindBestMove(t5, true, "a", 1);
-		hb.FindBestMove(t6, true, "a", 1);
-		hb.FindBestMove(t7, true, "a", 1);
-		hb.FindBestMove(t8, true, "a", 1);
-		hb.FindBestMove(t9, true, "a", 1);
-		hb.FindBestMove(t10, true, "a", 1);
-		hb.FindBestMove(t11, true, "a", 1);
-		hb.FindBestMove(t12, true, "a", 1);
-		hb.FindBestMove(t13, true, "a", 1);
-		hb.FindBestMove(t14, true, "a", 1);
-		hb.FindBestMove(t15, true, "a", 1);
-		hb.FindBestMove(t16, true, "a", 1);
-		hb.FindBestMove(t17, true, "a", 1);
-		hb.FindBestMove(t18, true, "a", 1);
-		hb.FindBestMove(t19, true, "a", 1);
-		hb.FindBestMove(t20, true, "a", 1);
-		hb.FindBestMove(t21, true, "a", 1);
-		hb.FindBestMove(t22, true, "a", 1);
-		hb.FindBestMove(t23, true, "a", 1);
-		hb.FindBestMove(t24, true, "a", 1);
-		hb.FindBestMove(t25, true, "a", 1);
-		hb.FindBestMove(t26, true, "a", 1);
-		hb.FindBestMove(t27, true, "a", 1);
-		hb.FindBestMove(t28, true, "a", 1);
-		hb.FindBestMove(t29, true, "a", 1);
-		hb.FindBestMove(t30, true, "a", 1);
-		hb.FindBestMove(t31, true, "a", 1);
-		hb.FindBestMove(t32, true, "a", 1);
-		hb.FindBestMove(t33, true, "a", 1);
-		hb.FindBestMove(t34, true, "a", 1);
-		hb.FindBestMove(t35, true, "a", 1);
-		hb.FindBestMove(t36, true, "a", 1);
-		hb.FindBestMove(t37, true, "a", 1);
-		hb.FindBestMove(t38, true, "a", 1);
-		
-		//THRESHOLD SET TO 8
-	}*/
-	
-	/*@Test
-	public void testStringos() {
-		String[] a = new String[0];
-		System.out.println(a.length);
-		
-	}*/
 	
 }
